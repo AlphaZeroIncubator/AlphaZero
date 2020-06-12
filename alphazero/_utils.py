1d164905129b00a1b294cd5cf19cfd7fb45fb7e8
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2020-06-11 19:25:09 -0500

    add custom loss function

---
+++ alphazero/_utils.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+import torch.nn as nn
+
+
+def AlphaGoZeroLoss(ground_truth, output, ratio=1):
+    """
+    Calculates AlphaGoZero style loss.
+    L2 regularization is included in the optimizer and would be very slow to
+    compute here, since it would require unpacking the parameters of the NN
+    at every step. So we're using it there instead.
+    """
+
+    mse = nn.MSELoss()
+    ce = nn.CrossEntropyLoss()
+
+    policy_true = ground_truth[0]
+    q_true = ground_truth[1]
+
+    policy = output[0]
+    q_value = output[1]
+
+    return mse(policy_true, policy) + ratio * ce(q_true, q_value)
