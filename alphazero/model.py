5c35bc946636b27425e16adf856ab0b4307ef976
abhon <39113839+abhon@users.noreply.github.com>
2020-06-15 16:07:19 -0700

    finish architecture, write tests later

--- alphazero/model.py
+++ alphazero/model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -11,27 +10,32 @@ class ResidualBlock(nn.Module):
         batch_on=True,
     ):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1)
-        self.bn = nn.BatchNorm2d(out_channels) if batch_on else None
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels) if batch_on else None
+        self.bn2 = nn.BatchNorm2d(out_channels) if batch_on else None
         self.activation = activation
 
     def forward(self, x):
         """
-        Create One Residual Block where we save the input 
+        Create One Residual Block where we save the input
         as the residual and add it after two convolutions
         """
         residual = x
         # first convolution
         out = self.conv1(x)
-        if self.batchnorm is not None:
-            out = self.bn(out)
+        if self.bn1 is not None:
+            out = self.bn1(out)
         out = self.activation(out)
 
         # second convolution, add input after batchnorm step
         out = self.conv2(out)
-        if self.bn is not None:
-            out = self.bn(out)
+        if self.bn2 is not None:
+            out = self.bn2(out)
         out += residual
         out = self.activation(out)
 
@@ -41,9 +45,9 @@ class ResidualBlock(nn.Module):
 class SidNet(nn.Module):
     """
     Creates the Encoding Neural Network. Takes
-            block: type of blocks to construct. In this case we use ResidualBlocks as defined above
-            in_channels: 3-D tensor of game.width*game.height*(number of previous turns and player positions)
-            enc_channels: number of channels to encode 
+            block: type of blocks to construct.(ResidualBlocks)
+            in_channels: # of game states(previous turns and positions)
+            enc_channels: number of channels to encode
             num_blocks: number of residual blocks to create
     """
 
@@ -57,16 +61,18 @@ class SidNet(nn.Module):
         batch_on=True,
     ):
         super(SidNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, enc_channels, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(
+            in_channels, enc_channels, kernel_size=3, stride=1, padding=1
+        )
         self.bn = nn.BatchNorm2d(enc_channels) if batch_on else None
         self.resblock = self.make_layer(block, enc_channels, num_blocks)
         self.activation = activation
 
-    def make_layer(self, block, out_channels, num_blocks, stride=1):
+    def make_layer(self, block, out_channels, num_blocks):
 
         layers = []
-        for i in range(0, num_layers):
-            layers.append(block(enc_channels, enc_channels, stride=1))
+        for i in range(0, num_blocks):
+            layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -82,74 +88,85 @@ class SidNet(nn.Module):
 
 class PolicyHead(nn.Module):
     """
-    Create the PolicyHead Network, which take the encoded features and the game being played 
-    from the previous SidNet Network and returns a vector of length game.width * game.height of 
-    move probabilities on each specific position. Following the architecture of the AlphaGoZero Paper,
-    only requires the number of channels from the previous residual nets and the game
-    being played to obtain board positions and applies the transformations mentioned below.
+    Create the PolicyHead Network, which take the encoded features and the game
+    being played from the previous SidNet Network and returns a vector of
+    length game.width * game.height of move probabilities for each specific
+    position. Following the architecture of the AlphaGoZero Paper, requires the
+    number of channels from the previous residual nets, the game being played,
+    the activation function, and if bathcnorm is on or off.
     """
 
     def __init__(
         self,
         enc_channels: int,
-        game: Game,
+        game,
         activation=nn.ReLU(inplace=False),
         batch_on=True,
+        pass_move=True,
     ):
 
         super(PolicyHead, self).__init__()
-        self.policyconv = nn.Conv2D(
-            enc_channels, out_channels=2, kernel_size=2, stride=1
+        self.policyconv = nn.Conv2d(
+            enc_channels, out_channels=2, kernel_size=1, stride=1
         )
         self.policybn = nn.BatchNorm2d(2) if batch_on else None
-        self.policyfc = nn.Linear(
-            in_channels=enc_channels, out_channels=game.width * game.height,
-        )
+        self.poss_moves = game.width * game.height
+
+        if pass_move:
+            self.policyfc = nn.Linear(
+                in_features=self.poss_moves * 2, out_features=self.poss_moves + 1
+            )
+        else:
+            self.policyfc = nn.Linear(
+                in_features=self.poss_moves * 2, out_features=self.poss_moves
+            )
         self.activation = activation
+        self.logsoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, encoding):
         policy = self.policyconv(encoding)
         if self.policybn is not None:
             policy = self.policybn(policy)
         policy = self.activation(policy)
-        # a step here I am not completely sure yet
+        policy = policy.view(-1, self.poss_moves * 2)
         policy = self.policyfc(policy)
-        return policy
+        probas = self.logsoftmax(policy).exp()
+        return probas
 
 
 class ValueHead(nn.Module):
     """
-    Create the ValueHead Network, which take the encoded features from the previous SidNet
-    Network and returns a value between -1 and 1 predicting the probability of winning. Following
-    the architecture of the AlphaGoZero Paper, only requires the number of channels from the
-    previous residual nets and applies the transformations mentioned below.
+    Create the ValueHead Network, which take the encoded features from the
+    SidNet Network and returns a value between -1 and 1 predicting the
+    probability of winning. Requres number of encoded features, the game being
+    played, the activation function, and if bathcnorm is on or off
     """
 
     def __init__(
-        self, enc_channels: int, activation=nn.ReLU(inplace=False), batch_on=True
+        self, enc_channels: int, game, activation=nn.ReLU(inplace=False), batch_on=True,
     ):
 
         super(ValueHead, self).__init__()
-        self.valueconv = nn.Conv2D(
+        self.valueconv = nn.Conv2d(
             enc_channels, out_channels=1, kernel_size=1, stride=1
         )
         self.valuebn = nn.BatchNorm2d(1) if batch_on else None
-
+        self.game_area = game.width * game.height
         # figuring out what the in_channel should be for this
-        self.valuefc1 = nn.Linear(in_channels=number, out_channels=256)
-        self.valuefc2 = nn.Linear(in_channels=256, out_channels=1)
+        self.valuefc1 = nn.Linear(in_features=self.game_area, out_features=256)
+        self.valuefc2 = nn.Linear(in_features=256, out_features=1)
         self.activation = activation
 
-        self.tanh = nn.tanh(inplace=False)
+        self.tanh = nn.Tanh()
 
     def forward(self, encoding):
         value = self.valueconv(encoding)
         if self.valuebn is not None:
             value = self.valuebn(value)
-        value = self.relu(value)
-        # a step here I am not completely sure yet
+        value = self.activation(value)
+        value = value.view(-1, self.game_area)
         value = self.valuefc1(value)
-        value = self.relu(value)
+        value = self.activation(value)
         value = self.valuefc2(value)
         win_value = self.tanh(value)
         return win_value
