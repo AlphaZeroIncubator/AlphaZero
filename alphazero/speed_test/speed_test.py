f1bb6e36e3607e0cb00e6f042b257b8ce52f3e1d
homerOurs <homerours@gmail.com>
2020-07-08 15:16:09 +0200

    run with TicTacToe

--- alphazero/speed_test/speed_test.py
+++ alphazero/speed_test/speed_test.py
@@ -1,24 +1,25 @@
 from alphazero.model import PhilipNet
 from alphazero.mcts import self_play
 from alphazero import Connect4
+from alphazero import TicTacToe
 from alphazero import LeoConverter
 
-n_mcts_iter = 1600
-temperature = 1e-4
+n_mcts_iter = 100
+temperature = 0.5
 dirichlet_eps = 0.25
 dirichlet_conc = 1.0
 
-game = Connect4()
+game = TicTacToe()
 
-model = PhilipNet(in_channels = 2, game = game)
-print(model(LeoConverter.board_to_tensor(game.board_state,1)))
+model = PhilipNet(in_channels = 2, game = game, num_blocks = 10, enc_channels = 128)
+# print(model(LeoConverter.board_to_tensor(game.board_state,1)))
 
-# my_game = self_play(
-        # game,
-        # model,
-        # board_converter=LeoConverter,
-        # n_mcts_iter=n_mcts_iter,
-        # temperature=temperature,
-        # dirichlet_eps=dirichlet_eps,
-        # dirichlet_conc=dirichlet_conc,
-        # )
+my_game = self_play(
+        game,
+        model,
+        board_converter=LeoConverter,
+        n_mcts_iter=n_mcts_iter,
+        temperature=temperature,
+        dirichlet_eps=dirichlet_eps,
+        dirichlet_conc=dirichlet_conc,
+        )
