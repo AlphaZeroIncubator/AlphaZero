c4d40dbd813eb1b4c2dfc829eb53cff16751cfec
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2020-07-19 14:12:07 -0500

    update alphagozeroloss test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,7 +15,7 @@ class TestUtils:
 class Test_Loss:
     def test_alphazero_input(self):
         policy_true = torch.Tensor([0.1, 0.1, 0.8])
-        q_true = torch.Tensor([[1000, 0]])
+        q_true = torch.Tensor([[0]])
 
         policy_pred = torch.Tensor([0.1, 0.1, 0.8])
         q_pred = torch.LongTensor([0])
@@ -23,13 +23,24 @@ class Test_Loss:
         truth = (policy_true, q_true)
         pred = (policy_pred, q_pred)
 
-        assert torch.allclose(AlphaGoZeroLoss(truth, pred), torch.Tensor([0]))
+        assert torch.allclose(
+            AlphaGoZeroLoss(truth, pred), torch.Tensor([0.689727])
+        )
 
         policy_pred = torch.Tensor([1.0, 0, 0])
-        q_pred = torch.LongTensor([1])
+        q_pred = torch.LongTensor([0])
 
         pred = (policy_pred, q_pred)
 
         assert torch.allclose(
-            AlphaGoZeroLoss(truth, pred), torch.Tensor([1000.4867])
+            AlphaGoZeroLoss(truth, pred), torch.Tensor([1.55144])
+        )
+
+        policy_pred = torch.Tensor([0.1, 0.1, 0.8])
+        q_pred = torch.LongTensor([2])
+
+        pred = (policy_pred, q_pred)
+
+        assert torch.allclose(
+            AlphaGoZeroLoss(truth, pred), torch.Tensor([4.689727])
         )
